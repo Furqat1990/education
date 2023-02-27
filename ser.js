ca887ebca848fb944c1eaebab8e566c21330c54d
Furqat1990 <nebula.01.01.1996@gmail.com>
2023-02-27 23:00:58 +0500

    complete Date

--- ser.js
+++ ser.js
@@ -6,13 +6,12 @@ function createDateShedule(startDateStr, period) {
     let startDate = new Date(startDateStr);
     let d = 0;
     let startDay = startDate.getDate();
-    let x = startDay;
+    let residualDays = startDay;
     let month;
     let year;
     let days;
     
     for (let i = 1; i <= period; i++) {
-
         if (month > 11) {
             startDate = new Date(`${++year}-01-01`);
             d = 0;
@@ -22,7 +21,7 @@ function createDateShedule(startDateStr, period) {
         month = startDate.getMonth() + ++d;
         days = new Date(year, month, 0).getDate();
 
-        if (i === period) days = x;
+        if (i === period) days = residualDays;
 
         for (let j = startDay; j <= days; j++) {
             console.log(j, month, year);
